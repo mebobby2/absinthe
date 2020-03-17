c378c7ddc4024630aa0591173349bb21269ea6a6
Bobby <bobby.lei@vodafone.com>
2020-03-17 21:15:50 +1300

    Add mix_test_watch

--- mix.exs
+++ mix.exs
@@ -55,6 +55,7 @@ defmodule PlateSlate.Mixfile do
       {:absinthe_plug, "~> 1.4.0"},
       {:absinthe_phoenix, "~> 1.4.0"},
       {:absinthe_relay, "~> 1.4.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 

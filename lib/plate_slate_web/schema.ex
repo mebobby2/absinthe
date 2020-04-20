24c52f2456dbc87c5d9a464d945840e32028ff36
Bobby <bobby.lei@vodafone.com>
2020-04-20 13:42:03 +1200

    Scope customer new order subscriptions on their own orders only

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -81,8 +81,15 @@ defmodule PlateSlateWeb.Schema do
 
   subscription do
     field :new_order, :order do
-      config fn _args, _info ->
-        {:ok, topic: "*"}
+      config fn _args, %{context: context} ->
+        case context[:current_user] do
+          %{role: "customer", id: id} ->
+            {:ok, topic: id}
+          %{role: "employee"} ->
+            {:ok, topic: "*"}
+          _ ->
+            {:error, "unauthorized"}
+        end
       end
     end
 
